83d332fe1a6fae2f631a2ba537accccb8181fe82
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2025-05-31 09:35:19 +0900

    add swagger

--- pkg/backend/src/app.controller.ts
+++ pkg/backend/src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get sample data' })
+  @ApiResponse({ status: 200, description: 'Success' })
   getHello(): string {
     return this.appService.getHello();
   }
--- pkg/backend/src/main.ts
+++ pkg/backend/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import * as fs from 'fs';
+import { dump } from 'js-yaml';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 4000);
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  fs.writeFileSync('./swagger-spec.yml', dump(documentFactory(), {}));
+  SwaggerModule.setup('api/docs', app, documentFactory);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
